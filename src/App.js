8c3dd93d1a73b066e400687fdb4c91fef4cef214
Jessica <Jessica@users-iMac.local>
2019-12-16 19:20:29 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterList from './components/CharacterList'; 
+import WelcomePage from './components/WelcomePage'; 
+import SearchForm from './components/SearchForm';
+import { Route, Link } from 'react-router-dom';
+import styled from 'styled-components';  
 
+const Navbar = styled.div `
+  // border: 2px solid red;
+  padding: 20px;
+  background: darkcyan;
+  color: white;
+  display: felx;
+  justify-content: center;
+ `; 
 
 export default function App() {
   return (
     <main>
+      <Navbar>
+        <Link className='navLink' to='/'>Home</Link>
+        <Link className='navLink' to='/characters'>Characters</Link>
+        <Link className='navLink' to='/search'>Search</Link>
+      </Navbar>
+
       <Header />
+      
+      <Route exact path='/' component={WelcomePage}/>
+      <Route path='/characters' component={CharacterList}/>
+      <Route path='/search' component={SearchForm}/>
+
     </main>
   );
 }
